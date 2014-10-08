a5f35500e84becdcc29bac3eb0aa33df8c0035b8
ocbenji <ocbenji@gmail.com>
2014-10-08 11:02:49 -0500

    TrekBikesTest

---
+++ TrekBikesTest/TrekBikesTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrekBikesTest.Startup))]
+namespace TrekBikesTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
